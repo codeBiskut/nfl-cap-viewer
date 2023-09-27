e045c29fbbd2fb9113e2304b5cf02705bc53215c
Joshua Rinehart <josh.rinehart.2k@gmail.com>
2023-09-27 17:06:46 -0400

    working to connect python script to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
+import React, { useState } from 'react';
+
 function App() {
+  const [output, setOutput] = useState('');
+
+  const runPythonScript = async () => {
+    try {
+      const response = await fetch('/get-contracts'); // Assumes the React app is hosted on the same server
+      console.log(response.data)
+      setOutput(response.data.output);
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle error in your React component
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={runPythonScript}>Run Python Script</button>
+      <div>
+        <pre>{output}</pre>
+      </div>
     </div>
   );
 }
