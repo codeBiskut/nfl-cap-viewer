d4a40ad409c2ee84e964d2a5f67ad2171298f4ef
Joshua Rinehart <josh.rinehart.2k@gmail.com>
2023-09-29 12:10:39 -0400

    changed scraper to target cap information instead of contracts

--- server/scraper.py
+++ server/scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import json
 
 def findContracts():# URL of the page to scrape
-    url = "https://www.spotrac.com/nfl/cleveland-browns/contracts/"
+    url = "https://www.spotrac.com/nfl/cleveland-browns/cap/"
 
     # Send an HTTP GET request to the URL
     response = requests.get(url)
@@ -12,32 +12,44 @@ def findContracts():# URL of the page to scrape
         # Parse the HTML content of the page
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Locate the contracts table on the page
-        contracts_table = soup.find('table', class_='datatable')
+        # Locate the individual cap table on the page
+        tables = soup.find_all('table')
 
-        if contracts_table:
+        if tables:
+            contract_table = tables[0]
             # Extract contract data from the table and organize it
             contract_data = []
-            for row in contracts_table.find_all('tr')[1:]:  # Skip the header row
+            for row in contract_table.find_all('tr')[1:]:  # Skip the header row
                 cells = row.find_all('td')
-                if len(cells) >= 7:
-                    contract_spans = cells[4].find_all('span')
-                    last_two_spans = [span.text for span in contract_spans[-2:]];
-                    combined_spans = last_two_spans[0] + ', ' + last_two_spans[1]
-                    player_name = cells[0].a.text.strip()
+                if len(cells) >= 12:
+                    player = cells[0].text.strip()
+                    player_name_parts = player.split("\n")
+                    player_name = player_name_parts[1] if len(player_name_parts) > 1 else ""
                     position = cells[1].text.strip()
-                    contract_value = combined_spans
-                    cap_hit = cells[5].text.strip()
-                    expires = cells[7].text.strip()
-                    acquired = cells[8].text.strip()
+                    base_salary = cells[2].text.strip()
+                    signing_bonus = cells[3].text.strip()
+                    roster_bonus = cells[4].text.strip()
+                    option_bonus = cells[5].text.strip()
+                    workout_bonus = cells[6].text.strip()
+                    restructuring_bonus = cells[7].text.strip()
+                    misc = cells[8].text.strip()
+                    dead_cap = cells[9].text.strip()
+                    cap_hit = cells[10].text.strip()
+                    cap_percent = cells[11].text.strip()
 
                     contract_data.append({
                         "Player": player_name,
                         "Position": position,
-                        "Contract Value": contract_value,
+                        "Base Salary": base_salary,
+                        "Signing Bonus": signing_bonus,
+                        "Roster Bonus": roster_bonus,
+                        "Option Bonus": option_bonus,
+                        "Workout Bonus": workout_bonus,
+                        "Restructuring Bonus": restructuring_bonus,
+                        "Misc": misc,
+                        "Dead Cap": dead_cap,
                         "Cap Hit": cap_hit,
-                        "Expires": expires,
-                        "Acquired": acquired
+                        "Cap Percent": cap_percent
                     })
 
             # Convert the data to JSON
@@ -46,8 +58,8 @@ def findContracts():# URL of the page to scrape
 
             # Print or return the JSON data
             # print(json_data)
-            # with open('browns_contracts.json', 'w') as file:
-            #     file.write(json_data)
+            with open('browns_contracts.json', 'w') as file:
+                file.write(json_data)
             return json_data
         else:
             print("Contracts table not found on the page.")
